f72b60ad1374b2b90d91bad95449a629f2fe96e2
Env-byte <tompenn18@gmail.com>
2022-08-22 22:16:57 +0100

    Update db structure

--- riot-backend/docs/db.sql
+++ riot-backend/docs/db.sql
@@ -1,5 +1,34 @@
-CREATE TABLE google_accounts (
-                       id              SERIAL PRIMARY KEY,
-                       title           VARCHAR(100) NOT NULL,
-                       primary_author  VARCHAR(100) NULL
+CREATE EXTENSION citext;
+
+CREATE SCHEMA general;
+
+CREATE TYPE login_method AS ENUM ('google');
+
+CREATE TABLE general.users
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    email      citext       NOT NULL,
+    type       login_method NOT NULL,
+    token      text
+);
+CREATE UNIQUE INDEX ON general.users ((lower(email)));
+
+CREATE SCHEMA tft;
+
+CREATE TABLE tft.teams
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    tft_set    smallint     NOT NULL,
+    created_by integer REFERENCES general.users (id),
+    create_on  timestamp
+);
+
+CREATE TABLE tft.team_champions
+(
+    championId varchar(50) PRIMARY KEY,
+    team_id    integer REFERENCES tft.teams (id),
+    item_ids   smallint[]
 );
