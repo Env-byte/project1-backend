3f669356393506f729e3474b34280bc1fafa5ede
Env-byte <tompenn18@gmail.com>
2022-10-02 13:29:00 +0100

    Update ErrorResponse.cs

--- riot-backend/Api/Responses/ErrorResponse.cs
+++ riot-backend/Api/Responses/ErrorResponse.cs
@@ -6,11 +6,13 @@ public class ErrorResponse
 {
     public string Type { get; set; }
     public string Message { get; set; }
+    public string Stack { get; set; }
 
     public ErrorResponse(Exception ex)
     {
         Type = ex.GetType().Name.Replace("Exception", "").SplitOnCapitals();
         var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        Message = env == "Development" ? ex.ToString() : ex.Message;
+        Message = ex.Message;
+        Stack = env == "Development" ? ex.ToString() : "";
     }
 }
